d9616b83b67793e302088638ad7f47ccfd6d3ae5
tammy vu <tammy.vsk@gmail.com>
2023-01-09 12:35:17 -0600

    int abs complete

--- DoubleOps.java
+++ DoubleOps.java
@@ -1,5 +1,7 @@
 class DoubleOps {
     public static void main(String[] args) {
-        
+        int absolute = Math.abs(Integer.parseInt(args[0]));
+        System.out.println(absolute);
+
     }
 }
