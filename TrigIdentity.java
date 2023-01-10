6fcdaeb0e8eb9805eb2046822fe3121e2803c5df
tammy vu <tammy.vsk@gmail.com>
2023-01-10 11:53:29 -0600

    rounded result

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -1,6 +1,6 @@
 class TrigIdentity {
     public static void main(String[] args) {
       double theta = Double.parseDouble(args[0]);
-      System.out.println((Math.pow(Math.sin(theta),2)) + (Math.pow(Math.cos(theta),2)));
+      System.out.println(Math.round((Math.pow(Math.sin(theta),2)) + (Math.pow(Math.cos(theta),2))));
     }
   }
