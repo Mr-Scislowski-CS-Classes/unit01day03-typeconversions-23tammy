158451c5f61c95bc2f7f5982806a03d95d7b0bb0
tammy vu <tammy.vsk@gmail.com>
2023-01-10 11:16:27 -0600

    finished

--- DoubleOps.java
+++ DoubleOps.java
@@ -1,7 +1,15 @@
 class DoubleOps {
     public static void main(String[] args) {
-        int absolute = Math.abs(Integer.parseInt(args[0]));
-        System.out.println(absolute);
-
+      int absolute = Math.abs(Integer.parseInt(args[0]));
+      System.out.println("int abs: " + absolute);
+  
+      double doublePow = Math.pow(Double.parseDouble(args[0]), Double.parseDouble(args[1]));
+      System.out.println("double pow: " + doublePow);
+  
+      double doubleSqrt = Math.sqrt(Double.parseDouble(args[0]));
+      System.out.println("double sqrt: " + doubleSqrt);
+  
+      double random = Math.random();
+      System.out.println("double random: " + random);
     }
-}
+  }
