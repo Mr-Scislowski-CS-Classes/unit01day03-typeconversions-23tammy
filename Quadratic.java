dad0f21a64c71a9ee21a90a68d7be500d9aa97bb
tammy vu <tammy.vsk@gmail.com>
2023-01-10 11:21:06 -0600

    fin

--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,15 @@
-public class Quadratic {
+class Quadratic {
     public static void main(String[] args) {
-        
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      int c = Integer.parseInt(args[2]);
+  
+      if (b*b - 4*a*c < 0) {
+        System.out.println("No real solutions.");
+      } else {
+      double x1 = (b*-1) + Math.sqrt((b*b - 4*a*c)/(2*a));
+      double x2 = (b*-1) - Math.sqrt((b*b - 4*a*c)/(2*a));
+      System.out.println("x: " + x1 +","+ x2);
+      }
     }
-}
+  }
